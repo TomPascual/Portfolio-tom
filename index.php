e467fab45e846b5929546ba03e57f83969fd4d23
Tom <tom.pascual@aftec.org>
2024-11-24 14:10:35 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html >
+<head>
+<title>PORTFOLIO TOM PASCUAL</title>
+<link rel="stylesheet" type="text/css "href="css/bienvenue.css" >
+</head>
+<body>
+    <h1>Bienvenue</h1>
+    <div class="d-flex flex-column justify-content-center w-100 h-100"></div>
+    <div class="box">
+        <div class="content">
+            <img src="images\img.png    ">
+        </div>
+    </div>
+    <a href="portfolio.html" class="btn">ENTRER</a>
+</body>
+
+</body>
+</html>
